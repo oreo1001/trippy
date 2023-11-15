86895f0f23575cd006fd6e965bce005709f91ef8
oreo <ohsimon77@naver.com>
2023-11-15 13:38:19 +0900

    기본 세팅

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
   //, required this.title
@@ -21,9 +22,19 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('안녕하세요'),
+        title: Row(
+          children: <Widget>[
+            Image.asset(
+              'assets/images/logo.png',  // 로고 이미지 경로를 입력하세요.
+              fit: BoxFit.contain,
+              height: 32.h,  // 로고의 높이를 설정하세요.
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 2.h,horizontal: 8.h),  // 로고와 이름 사이의 간격을 조절하세요.
+              child: Text('Trippy'),  // 앱 이름을 입력하세요.
+            )
+          ],
+        ),
       ),
       body: Center(
         child: Column(
